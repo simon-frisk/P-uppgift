e46108eaa258d5ff2e6ed5b6280c56937796f771
Simon Frisk <simon@simonfrisk.com>
2020-11-26 19:44:44 +0100

    Start implementing pygame_ui and move generate new graph functionality to ui button

--- gui.py
+++ gui.py
@@ -2,43 +2,58 @@ import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 
 import pygame
+import pygame_gui
 import math
 
 
 def createGUI(graph):
     FPS = 50
-    windowHeight = 650
-    windowWidth = round((graph.width / graph.height) * windowHeight)
-    boxWidth = windowWidth // graph.width
+    guiWidth = 250
+    seaHeight = 650
+    seaWidth = round((graph.width / graph.height) * seaHeight)
+    nodeWidth = seaWidth // graph.width
+    screenDimensions = (seaWidth + guiWidth, seaHeight)
 
     pygame.init()
-    display_surface = pygame.display.set_mode((windowWidth, windowHeight))
+    display_surface = pygame.display.set_mode(screenDimensions)
     pygame.display.set_caption('Sailing')
     clock = pygame.time.Clock()
-    font = pygame.font.SysFont(None, round(boxWidth / 2.5))
+    font = pygame.font.SysFont(None, round(nodeWidth / 2.5))
+    uiManager = pygame_gui.UIManager(screenDimensions)
 
     windArrow = pygame.image.load('assets/arrow.png')
-    windArrow = pygame.transform.scale(windArrow, (boxWidth, boxWidth))
+    windArrow = pygame.transform.scale(windArrow, (nodeWidth, nodeWidth))
+
+    newGraphButton = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect((seaWidth + 10, 10), (guiWidth - 20, 40)),
+        text='Generate new graph',
+        manager=uiManager
+    )
 
     while True:
+        time_delta = clock.tick(FPS) / 1000.0
+
         for event in pygame.event.get():
             pressed = pygame.key.get_pressed()
             if event.type == pygame.QUIT:
                 pygame.quit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                row = event.pos[1] // boxWidth
-                column = event.pos[0] // boxWidth
+                row = event.pos[1] // nodeWidth
+                column = event.pos[0] // nodeWidth
                 if pressed[pygame.K_s]:
                     graph.setStart((column, row))
                 if pressed[pygame.K_g]:
                     graph.setGoal((column, row))
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_r:
+            if event.type == pygame.USEREVENT:
+                if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                 if event.ui_element == newGraphButton:
                     graph.generateRandom(graph.width, graph.height)
+            uiManager.process_events(event)
 
-        renderGraph(graph, display_surface, windArrow, font, boxWidth)
+        renderGraph(graph, display_surface, windArrow, font, nodeWidth)
+        uiManager.update(time_delta)
+        uiManager.draw_ui(display_surface)
         pygame.display.update()
-        clock.tick(FPS)
 
 def renderGraph(graph, surface, image, font, width):
     for index, node in enumerate(graph.nodes):
